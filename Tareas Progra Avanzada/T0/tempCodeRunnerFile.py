0ef162fe8d5d99aaf933942f04ee0a8db132a436
mathimadsen17 <69520547+mathimadsen17@users.noreply.github.com>
2021-10-15 18:02:02 -0300

    Delete Tareas Progra Avanzada directory

--- Tareas Progra Avanzada/T0/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-    while len(lf) > 0:
-        for a in range(len(lf)):
-            if lf[a][3] < mas_antiguo:
-                mas_antiguo = lf[a][3]
-        for b in range(len(lf)):
-            if lf[b][3] == mas_antiguo:
-                lista_en_orden.append(lf[b])
-                lf.pop(b)
